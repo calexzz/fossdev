045ad5f3beeaf8a0db8df361f3305d8a20bcd1e6
alejandrogurtsov <alex.1046296@gmail.com>
2026-03-04 15:01:14 +0800

    Showed enough-test-case

--- testing/tests/test_principles.py
+++ testing/tests/test_principles.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('../src')
 
-from math_demo import add, add_with_bug
+from math_demo import add, add_with_bug, add_something
 
 def test_addition_basic():
     assert add(2,2) == 4, "Function didn't returned 4"
@@ -25,8 +25,36 @@ def test_addition_dublicated_logic():
     assert add(6,3) == 9
     print("Test DUBLICATED LOGIC PASSED")
 
+def test_addition_overcomplicated():
+    for i in range(0, 2**32):
+        for j in range(0, 2**32):
+            assert add(i,j) == i+j
+            assert add(-i, j) == -i + j
+            assert add(i, -j) == i - j
+            assert add(-i, -j) == -i - j
+
+def test_addition_reasonable():
+    assert add(6,3 ) == 9
+    assert add(0, 3) == 3
+    assert add(0, -3) == -3
+    assert add(-7, 83) == 76
+    assert add(-7, -83) == -90
+    print("Test ADDITION REASONABLE PASSED")
+
+def test_add_something_reasonable():
+    assert add_something(6,3) == 9
+    assert add_something(None,None) == 0
+    assert add_something(None,"abc") == 0
+    assert add_something(None, 10) == 0
+    assert add_something("abc", "10") == "abc10"
+    assert add_something("10", "abc") == "10abc"
+    assert add_something("xyz", "abc") == "xyzabc"
+    print("Test ANOTHER ADDITION REASONABLE PASSED")
+
 if __name__ == '__main__':
     test_addition_basic()
     test_bug_addition_notsufficent()
     test_addition_dublicated_logic()
     test_bug_addition_enough()
+    test_addition_reasonable()
+    test_add_something_reasonable()
