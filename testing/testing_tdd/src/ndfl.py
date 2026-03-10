c8c3f3f330f1f0f2bb7d32a7fc9d9d581b7f7286
alejandrogurtsov <alex.1046296@gmail.com>
2026-03-10 16:53:35 +0800

    Refactored ndfl calculation

--- testing/testing_tdd/src/ndfl.py
+++ testing/testing_tdd/src/ndfl.py
@@ -1,4 +1,16 @@
 def calculate_tax(income):
+    tiers = [
+        (0.,0.,0.13),
+        (2_400_000,312_000,0.15),
+        (5_000_000,702_000,0.18),
+        (20_000_000,3_402_000,0.20),
+        (50_000_000,9_402_000,0.22),
+    ]
+    for start, addition, taxrate in tiers[::-1]:
+        if income > start:
+            return addition + (income - start) * taxrate
+    return
+
     if income < 2_400_000:
         return income * 0.13
     elif income < 5_000_000:
